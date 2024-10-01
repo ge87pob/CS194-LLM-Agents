ce6912fcb06d3c6fd37c3fd93e0b263e8fef2237
Jan Chen <jan618chen@gmail.com>
2024-10-01 00:28:56 -0700

    modified:   Lab_1/lab01_release/main.py

--- Lab_1/lab01_release/main.py
+++ Lab_1/lab01_release/main.py
@@ -61,4 +61,3 @@ def main(user_query: str):
 if __name__ == "__main__":
     assert len(sys.argv) > 1, "Please ensure you include a query for some restaurant when executing main."
     main(sys.argv[1])
-    
